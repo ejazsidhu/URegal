b09003bc22f56ea31d6427e7780db114f9bfb960
ejazsidhu <ejazsidhu@gmail.com>
2018-11-17 18:58:23 +0500

    tree structure added

--- src/app/private/network/network-tree/network-tree.component.ts
+++ src/app/private/network/network-tree/network-tree.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TreeNode } from 'primeng/primeng';
 
 @Component({
   selector: 'app-network-tree',
@@ -6,10 +7,90 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./network-tree.component.css']
 })
 export class NetworkTreeComponent implements OnInit {
-
+  files: TreeNode[];
   constructor() { }
 
   ngOnInit() {
+    this.files = [
+      {
+        label: 'Folder 1',
+        collapsedIcon: 'fa-folder',
+        expandedIcon: 'fa-folder-open',
+        children: [
+          {
+            label: 'child Folder 1',
+            collapsedIcon: 'fa-folder',
+            expandedIcon: 'fa-folder-open',
+            children: [
+              {
+                label: 'File 123',
+                icon: 'fa-file-o'
+              }
+            ]
+          },
+          {
+            label: 'childe Folder 2',
+            collapsedIcon: 'fa-folder',
+            expandedIcon: 'fa-folder-open',
+
+            children: [
+              {
+                label: 'File 12345',
+                icon: 'fa-file-o'
+              },
+              {
+                label: 'File 2',
+                icon: 'fa-file-o'
+              }
+            ]
+          }
+          // ,
+          // {
+          //   label: 'File 1',
+          //   icon: 'fa-file-o'
+          // }
+        ]
+      },
+      {
+        label: 'Parent Folder 2',
+        collapsedIcon: 'fa-folder',
+        expandedIcon: 'fa-folder-open',
+        children: [
+          {
+            label: 'child Folder 1',
+            collapsedIcon: 'fa-folder',
+            expandedIcon: 'fa-folder-open',
+            children: [
+              {
+                label: 'File 123',
+                icon: 'fa-file-o'
+              }
+            ]
+          },
+          {
+            label: 'childe Folder 2',
+            collapsedIcon: 'fa-folder',
+            expandedIcon: 'fa-folder-open',
+
+            children: [
+              {
+                label: 'File 12345',
+                icon: 'fa-file-o'
+              },
+              {
+                label: 'File 2',
+                icon: 'fa-file-o'
+              }
+            ]
+          }
+          // ,
+          // {
+          //   label: 'File 1',
+          //   icon: 'fa-file-o'
+          // }
+        ]
+      }
+    ];
   }
 
 }
--- src/app/private/network/team-tree/team-tree.component.ts
+++ src/app/private/network/team-tree/team-tree.component.ts
@@ -1,15 +1,96 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TreeNode } from 'primeng/primeng';
 @Component({
   selector: 'app-team-tree',
   templateUrl: './team-tree.component.html',
   styleUrls: ['./team-tree.component.css']
 })
 export class TeamTreeComponent implements OnInit {
+  files: TreeNode[];
 
   constructor() { }
 
   ngOnInit() {
+    this.files = [
+      {
+        label: 'Folder 1',
+        collapsedIcon: 'fa-folder',
+        expandedIcon: 'fa-folder-open',
+        children: [
+          {
+            label: 'child Folder 1',
+            collapsedIcon: 'fa-folder',
+            expandedIcon: 'fa-folder-open',
+            children: [
+              {
+                label: 'File 123',
+                icon: 'fa-file-o'
+              }
+            ]
+          },
+          {
+            label: 'childe Folder 2',
+            collapsedIcon: 'fa-folder',
+            expandedIcon: 'fa-folder-open',
+
+            children: [
+              {
+                label: 'File 12345',
+                icon: 'fa-file-o'
+              },
+              {
+                label: 'File 2',
+                icon: 'fa-file-o'
+              }
+            ]
+          }
+          // ,
+          // {
+          //   label: 'File 1',
+          //   icon: 'fa-file-o'
+          // }
+        ]
+      },
+      {
+        label: 'Parent Folder 2',
+        collapsedIcon: 'fa-folder',
+        expandedIcon: 'fa-folder-open',
+        children: [
+          {
+            label: 'child Folder 1',
+            collapsedIcon: 'fa-folder',
+            expandedIcon: 'fa-folder-open',
+            children: [
+              {
+                label: 'File 123',
+                icon: 'fa-file-o'
+              }
+            ]
+          },
+          {
+            label: 'childe Folder 2',
+            collapsedIcon: 'fa-folder',
+            expandedIcon: 'fa-folder-open',
+
+            children: [
+              {
+                label: 'File 12345',
+                icon: 'fa-file-o'
+              },
+              {
+                label: 'File 2',
+                icon: 'fa-file-o'
+              }
+            ]
+          }
+          // ,
+          // {
+          //   label: 'File 1',
+          //   icon: 'fa-file-o'
+          // }
+        ]
+      }
+    ];
   }
 
 }
