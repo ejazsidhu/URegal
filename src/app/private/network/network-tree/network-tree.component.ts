edeb6bbea2944e7c7abb0e8833f51ee80a5e0f9a
ejazsidhu <ejazsidhu@gmail.com>
2018-11-17 19:21:14 +0500

    miner changes done

--- src/app/private/network/network-tree/network-tree.component.ts
+++ src/app/private/network/network-tree/network-tree.component.ts
@@ -50,46 +50,8 @@ export class NetworkTreeComponent implements OnInit {
           //   icon: 'fa-file-o'
           // }
         ]
-      },
-      {
-        label: 'Parent Folder 2',
-        collapsedIcon: 'fa-folder',
-        expandedIcon: 'fa-folder-open',
-        children: [
-          {
-            label: 'child Folder 1',
-            collapsedIcon: 'fa-folder',
-            expandedIcon: 'fa-folder-open',
-            children: [
-              {
-                label: 'File 123',
-                icon: 'fa-file-o'
-              }
-            ]
-          },
-          {
-            label: 'childe Folder 2',
-            collapsedIcon: 'fa-folder',
-            expandedIcon: 'fa-folder-open',
-
-            children: [
-              {
-                label: 'File 12345',
-                icon: 'fa-file-o'
-              },
-              {
-                label: 'File 2',
-                icon: 'fa-file-o'
-              }
-            ]
-          }
-          // ,
-          // {
-          //   label: 'File 1',
-          //   icon: 'fa-file-o'
-          // }
-        ]
       }
+      
     ];
   }
 
--- src/app/private/network/team-tree/team-tree.component.ts
+++ src/app/private/network/team-tree/team-tree.component.ts
@@ -50,45 +50,6 @@ export class TeamTreeComponent implements OnInit {
           //   icon: 'fa-file-o'
           // }
         ]
-      },
-      {
-        label: 'Parent Folder 2',
-        collapsedIcon: 'fa-folder',
-        expandedIcon: 'fa-folder-open',
-        children: [
-          {
-            label: 'child Folder 1',
-            collapsedIcon: 'fa-folder',
-            expandedIcon: 'fa-folder-open',
-            children: [
-              {
-                label: 'File 123',
-                icon: 'fa-file-o'
-              }
-            ]
-          },
-          {
-            label: 'childe Folder 2',
-            collapsedIcon: 'fa-folder',
-            expandedIcon: 'fa-folder-open',
-
-            children: [
-              {
-                label: 'File 12345',
-                icon: 'fa-file-o'
-              },
-              {
-                label: 'File 2',
-                icon: 'fa-file-o'
-              }
-            ]
-          }
-          // ,
-          // {
-          //   label: 'File 1',
-          //   icon: 'fa-file-o'
-          // }
-        ]
       }
     ];
   }
