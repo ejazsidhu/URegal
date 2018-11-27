4be7a276d07a4f5e0fab17fb0690beb999a1730f
ejazsidhu <ejazsidhu@gmail.com>
2018-11-27 18:11:04 +0500

    error message added

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -12,12 +12,17 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
+  loading=false;
   @ViewChild("f") form: any;
+  message: any=[];
   constructor(public publicService: PublicService, public router: Router, private authService: AuthService) {
 
     this.publicService.user = new User();
   }
 
+  setAlertOff(){
+    this.message='';
+  }
   ngOnInit() {
     this.resetForm();
   }
@@ -28,21 +33,31 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit() {
+    this.loading=true
     if (this.form.valid) {
+      
+
       // console.log('obsubmit called', this.form.value)
       // if(this.form.value.Username=='aaram@gmail.com' && this.form.value.Password=="aaram")
       this.authService.login(this.form.value.Username, this.form.value.Password).subscribe(data => {
         localStorage.setItem('user', data);
+        console.log('data',data)
         let token_type = data.token_type;
         let access_token = data.access_token;
         let tokenKey = "" + token_type + " " + access_token + "";
         localStorage.setItem('tokenKey', tokenKey);
         // console.log(data)
+        this.loading=false
 
-        this.router.navigateByUrl('/home');
+        // this.router.navigateByUrl('/home');
 
       }, error => {
 
+        console.log('error',error);
+        this.loading=false;
+        let e=JSON.parse(error._body);
+        this.message=e.error_description
+        
       });
 
 
