71b4eb4fd4fbcda856bb9a07d5f1ecc54add75f1
affanahmad <affanahmaddev@gmail.com>
2018-12-01 18:03:20 +0500

    Clients Changes All Done

--- src/app/private/visa-card/visa-card.component.ts
+++ src/app/private/visa-card/visa-card.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visa-card.component.css']
 })
 export class VisaCardComponent implements OnInit {
-
+ card1:boolean = false;
+ card2:boolean = false;
+ card3:boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  cardperfect(){
+    this.card1 = true;
+  }
+  cardgold(){
+    this.card2 = true;
+  }
+  cardskrill(){
+    this.card3 = true;
+  }
 }
